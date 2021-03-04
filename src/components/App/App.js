1c6271c9fab38be81270b95c306a73736963c688
mainlyetcetera <ericcampbell294@gmail.com>
2021-03-04 15:30:45 -0700

    Render Message in App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from '../Header/Header.js'
+import Message from '../Message/Message.js'
 import './App.css'
 
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
     <div>
       <h1>App</h1>
       <Header />
+      <Message />
     </div>
   )
 }
---
+++ src/components/Message/Message.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Message = () => {
+  return (
+    <h3>Message</h3>
+  )
+}
+
+export default Message
