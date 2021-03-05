8378b26f8d55bec927105327d01ab7cffafe927f
mainlyetcetera <ericcampbell294@gmail.com>
2021-03-05 12:04:13 -0700

    Receive data in Details

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import './Details.css'
 
-const Details = () => (
-  <section>
-    <article>Quote again</article>
-    <article>Author</article>
-    <article>Categories</article>
-  </section>
-)
+const Details = ({info}) => {
+  const {content, author, tags} = info
+  return (
+    <section>
+      <article>{content}</article>
+      <article>{author}</article>
+      <article>{tags}</article>
+    </section>
+  )
+}
 
 export default Details
 
