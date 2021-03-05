4be3aba45a39bbc1d3f994eb419ddba1b86c2ba9
mainlyetcetera <ericcampbell294@gmail.com>
2021-03-05 10:43:34 -0700

    Build basic Details component

---
+++ src/components/Details/Details.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Details.css'
+
+const Details = () => (
+  <section>
+    <article>Quote again</article>
+    <article>Author</article>
+    <article>Categories</article>
+  </section>
+)
+
+export default React
+
