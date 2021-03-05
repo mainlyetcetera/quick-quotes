87fb4a58b08281922aba8f55964a4678d64e3ef1
mainlyetcetera <ericcampbell294@gmail.com>
2021-03-05 12:55:19 -0700

    Render Error

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,11 @@
 const fetchData = async url => {
   const res = await fetch(url)
+  if (!res.ok) {
+    const msg = `An error has occurred: ${res.status} ${res.statusText}`
+    console.log('error', msg)
+    throw new Error(msg)
+  }
+
   const data = await res.json()
   return data
 }
