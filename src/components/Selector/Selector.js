0484aa48c9b99128a57141a84ace810619492810
mainlyetcetera <ericcampbell294@gmail.com>
2021-03-04 17:12:20 -0700

    Fetch, generate, and display categories

--- src/components/Selector/Selector.js
+++ src/components/Selector/Selector.js
@@ -4,17 +4,46 @@ import { fetchData } from '../../utils/api.js'
 
 const Selector = () => {
   const [quote, setQuote] = useState(null)
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    const generateCategories = async () => {
+      const url = 'https://api.quotable.io/tags'
+      const data = await fetchData(url)
+      setCategories(data)
+    }
+
+    generateCategories()
+  }, [])
 
   useEffect(() => {
     const result = async () => {
       const url = 'https://api.quotable.io/random'
       const data = await fetchData(url)
-      console.log(data)
       setQuote(data.content)
     }
 
     result()
   }, [])
+
+  const displayCategories = () => {
+    return categories.map(category => {
+      const {name, quoteCount, _id} = category
+      let result
+      if (quoteCount) { 
+        result = (
+          <option 
+            value={name}
+            key={_id}
+          >
+          {name}
+          </option>
+        )
+      }
+
+      return result
+    })
+  }
 
   return (
     <div>
@@ -26,6 +55,9 @@ const Selector = () => {
           <option value=''>
             Please pick a category
           </option>
+          {
+            displayCategories()
+          }
         </select>
       </form>
     </div>
