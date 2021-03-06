83c3b7567ad82b8089f154321f7593196abbf8b6
mainlyetcetera <ericcampbell294@gmail.com>
2021-03-06 15:31:30 -0700

    Add skeleton tests based on stories

---
+++ cypress/integration/detail-spec.js
@@ -0,0 +1,7 @@
+describe('the details view', () => {
+  it('should show the details of the quote', () => {
+  })
+
+  it('should be able to return to the main view', () => {
+  })
+})
---
+++ cypress/integration/main-spec.js
@@ -0,0 +1,10 @@
+describe('the main view', () => {
+  it('should render a header with a greeting', () => {
+  })
+
+  it('should be able to search for quotes by category', () => {
+  })
+
+  it('should see an error message when the quotes don\'t load', () => {
+  })
+})
