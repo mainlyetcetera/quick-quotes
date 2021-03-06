aa7f5a79d385e8eed2c5f8a72304fc5d50ed6a71
mainlyetcetera <ericcampbell294@gmail.com>
2021-03-06 11:09:40 -0700

    Use status code and text in error msg

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
-const Error = () => (
-  <h1>Real Error...</h1>
+const Error = ({status, text}) => (
+  <section>
+    <article>Sorry!</article>
+    <article>Something went wrong!</article>
+    <article>{status}</article>
+    <article>{text}</article>
+  </section>
 )
 
 export default Error
