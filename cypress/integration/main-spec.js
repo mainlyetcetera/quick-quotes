456f54660dec5b6894d5b358a6b6ed1eec460d37
mainlyetcetera <ericcampbell294@gmail.com>
2021-03-06 16:02:54 -0700

    Intercept data for categories and a quote

--- cypress/integration/main-spec.js
+++ cypress/integration/main-spec.js
@@ -1,5 +1,29 @@
 describe('the main view', () => {
+  beforeEach(() => {
+    cy
+      .fixture('../fixtures/categories.json')
+      .then(data => {
+        cy.intercept('GET', 'https://api.quotable.io/tags', {
+          statusCode: 200,
+          body: data
+        })
+      })
+
+    cy
+      .fixture('../fixtures/quote.json')
+      .then(data => {
+        cy.intercept('GET', 'https://api.quotable.io/random?tags=wisdom', {
+          statusCode: 200,
+          body: data
+        })
+      })
+
+    cy
+      .visit('http://localhost:3000')
+  })
+
   it('should render a header with a greeting', () => {
+    expect(true).to.eql(true)
   })
 
   it('should be able to search for quotes by category', () => {
