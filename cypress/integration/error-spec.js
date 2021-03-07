d91a0a6ae830508f547c5c762fb4838063e7adb5
mainlyetcetera <ericcampbell294@gmail.com>
2021-03-07 15:48:51 -0700

    Add test, render 404 error

---
+++ cypress/integration/error-spec.js
@@ -0,0 +1,62 @@
+describe('quote errors', () => {
+  beforeEach(() => {
+    cy
+      .fixture('../fixtures/categories.json')
+      .then(data => {
+        cy.intercept('GET', 'https://api.quotable.io/tags', {
+          statusCode: 200,
+          body: data
+        })
+      })
+
+    cy
+      .fixture('../fixtures/wisdom-quote.json')
+      .then(data => {
+        cy.intercept('GET', 'https://api.quotable.io/random?tags=wisdom', {
+          statusCode: 404
+        })
+      })
+
+    cy
+      .fixture('../fixtures/biz-quote.json')
+      .then(data => {
+        cy.intercept('GET', 'https://api.quotable.io/random?tags=business', {
+          statusCode: 501
+        })
+      })
+    
+    cy
+      .visit('http://localhost:3000')
+  })
+
+  it('should render a 404 error', () => {
+    cy
+      .get('h3')
+      .contains('Something clever will go here soon...')
+
+      .get('select')
+      .select('Wisdom').invoke('val')
+
+      .get('select')
+      .should('have.value', 'wisdom')
+
+      .get('button')
+      .click()
+
+      .get('section')
+      .children('article:first')
+      .contains('Sorry!')
+
+      .get('section')
+      .children('article:nth-child(2)')
+      .contains('Something went wrong!')
+
+      .get('section')
+      .children('article:nth-child(3)')
+      .contains('404')
+
+      .get('section')
+      .children('article:nth-child(4)')
+      .contains('Not Found')
+  })
+})
