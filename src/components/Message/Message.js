4c0a22a79c1ddad0168e8c6c3f5db917846f5976
mainlyetcetera <ericcampbell294@gmail.com>
2021-03-07 16:20:47 -0700

    Add prop type check to Message

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import './Message.css'
 
@@ -22,3 +23,7 @@ const Message = ({quote}) => {
 }
 
 export default Message
+
+Message.propTypes = {
+  quote: PropTypes.string
+}
