70a5ed19988d8bdb3104921b53abf9da17a1815d
mainlyetcetera <ericcampbell294@gmail.com>
2021-03-07 15:51:08 -0700

    Add test, render 501 error

--- cypress/integration/error-spec.js
+++ cypress/integration/error-spec.js
@@ -59,4 +59,35 @@ describe('quote errors', () => {
       .children('article:nth-child(4)')
       .contains('Not Found')
   })
+
+  it('should render a 501 error', () => {
+    cy
+      .get('h3')
+      .contains('Something clever will go here soon...')
+
+      .get('select')
+      .select('Business').invoke('val')
+
+      .get('select')
+      .should('have.value', 'business')
+
+      .get('button')
+      .click()
+
+      .get('section')
+      .children('article:first')
+      .contains('Sorry!')
+
+      .get('section')
+      .children('article:nth-child(2)')
+      .contains('Something went wrong!')
+
+      .get('section')
+      .children('article:nth-child(3)')
+      .contains('501')
+
+      .get('section')
+      .children('article:nth-child(4)')
+      .contains('Not Implemented')
+  })
 })
